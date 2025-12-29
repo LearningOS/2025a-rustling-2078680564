8c801fb8aa02572c502e1f93ecc2959da91ccb82
2078680564 <2078680564@qq.com>
2025-12-29 12:55:51 +0000

    update

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -11,8 +11,8 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    pub use self::fruits::PEAR as fruit;
-    pub use self::veggies::CUCUMBER as veggie;
+    use self::fruits::PEAR as ???
+    use self::veggies::CUCUMBER as ???
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
