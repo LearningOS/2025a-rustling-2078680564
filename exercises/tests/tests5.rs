b392c182ba6cfe1f4aabb5540b69ca142bbfdc0a
2078680564 <2078680564@qq.com>
2025-12-29 13:04:00 +0000

    update

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -28,11 +28,11 @@
 ///
 /// The `address` must contain a mutable reference to a valid `u32` value.
 unsafe fn modify_by_address(address: usize) {
-    // SAFETY: The address is guaranteed to be valid and contains
-    // a unique reference to a `u32` value, as per the function's safety contract.
+    // TODO: Fill your safety notice of the code block below to match your
+    // code's behavior and the contract of this function. You may use the
+    // comment of the test below as your format reference.
     unsafe {
-        let ptr = address as *mut u32;
-        *ptr = 0xAABBCCDD;
+        todo!("Your code goes here")
     }
 }
 
