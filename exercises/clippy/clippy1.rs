ef01c7ec52f81d98bce7f3dfbc304e2e92e66b8b
2078680564 <2078680564@qq.com>
2025-12-29 12:41:12 +0000

    update

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -11,14 +11,16 @@
 
 // I AM NOT DONE
 
-use std::f32::consts::PI;
+use std::f32;
 
 fn main() {
-    let radius = 5.00;
-    let area = PI * radius * radius;
+    let pi = 3.14f32;
+    let radius = 5.00f32;
+
+    let area = pi * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
         radius, area
-    );
+    )
 }
