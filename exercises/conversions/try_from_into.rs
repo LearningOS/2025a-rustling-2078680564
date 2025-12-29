36ad1ec123dd4afe46fd1f318a95d04a3f843430
2078680564 <2078680564@qq.com>
2025-12-29 12:43:17 +0000

    update

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -61,7 +61,7 @@ impl TryFrom<(i16, i16, i16)> for Color {
     }
 }
 
-// 2. 实现从[i16; 3]数组转换为Color
+// Array implementation
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
@@ -74,7 +74,7 @@ impl TryFrom<[i16; 3]> for Color {
     }
 }
 
-// 3. 实现从&[i16]切片转换为Color
+// Slice implementation
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
@@ -91,18 +91,21 @@ impl TryFrom<&[i16]> for Color {
 }
 
 fn main() {
+    // Use the `try_from` function
     let c1 = Color::try_from((183, 65, 14));
-    println!("{:?}", c1); // Ok(Color { red: 183, green: 65, blue: 14 })
+    println!("{:?}", c1);
 
+    // Since TryFrom is implemented for Color, we should be able to use TryInto
     let c2: Result<Color, _> = [183, 65, 14].try_into();
-    println!("{:?}", c2); // Ok(Color { red: 183, green: 65, blue: 14 })
+    println!("{:?}", c2);
 
     let v = vec![183, 65, 14];
+    // With slice we should use `try_from` function
     let c3 = Color::try_from(&v[..]);
-    println!("{:?}", c3); // Ok(Color { red: 183, green: 65, blue: 14 })
-
+    println!("{:?}", c3);
+    // or take slice within round brackets and use TryInto
     let c4: Result<Color, _> = (&v[..]).try_into();
-    println!("{:?}", c4); // Ok(Color { red: 183, green: 65, blue: 14 })
+    println!("{:?}", c4);
 }
 
 #[cfg(test)]
