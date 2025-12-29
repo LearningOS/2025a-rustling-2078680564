882d7b272d4c9c0a11cf7da495fca18d55a8a460
2078680564 <2078680564@qq.com>
2025-12-29 12:43:41 +0000

    update

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -6,15 +6,12 @@
 
 #[derive(Debug)]
 enum Message {
-    Quit,
-    Echo(String),               // 携带字符串的变体
-    Move { x: i32, y: i32 },    // 携带匿名结构体的变体
-    ChangeColor(u8, u8, u8),    // 携带三个 u8 类型（RGB）的变体
+    // TODO: define a few types of messages as used below
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo(String::from("Hello")));
-    println!("{:?}", Message::Move { x: 10, y: 20 });
-    println!("{:?}", Message::ChangeColor(255, 0, 0));
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
 }
