730c7423778153ed9945538152f091b0eb9b65a8
2078680564 <2078680564@qq.com>
2025-12-29 12:46:35 +0000

    update

--- exercises/error_handling/errors5.rs
+++ exercises/error_handling/errors5.rs
@@ -28,15 +28,16 @@ use std::error;
 use std::fmt;
 use std::num::ParseIntError;
 
-// 修复返回类型：Box<dyn error::Error> 能容纳所有实现了 Error trait 的错误类型
-fn main() -> Result<(), Box<dyn error::Error>> {
+// TODO: update the return type of `main()` to make this compile.
+fn main() -> Result<(), Box<dyn ???>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
     Ok(())
 }
 
-// 以下代码无需修改
+// Don't change anything below this line.
+
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -56,6 +57,7 @@ impl PositiveNonzeroInteger {
     }
 }
 
+// This is required so that `CreationError` can implement `error::Error`.
 impl fmt::Display for CreationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let description = match *self {
